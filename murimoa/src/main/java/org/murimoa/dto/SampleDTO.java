5462a46f9cb4c0d8e139e4e5b0d28fe68cf16dd3
hsw <hsw@DESKTOP-ANTKN9A>
2018-08-11 03:11:33 +0900

    Lombok

---
+++ murimoa/src/main/java/org/murimoa/dto/SampleDTO.java
@@ -0,0 +1,10 @@
+package org.murimoa.dto;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	private String test;
+    private Date regdate;
+}
--- murimoa/src/test/java/org/murimoa/web/DBTest.java
+++ murimoa/src/test/java/org/murimoa/web/DBTest.java
@@ -8,14 +8,19 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.murimoa.dto.SampleDTO;
+import org.murimoa.mapper.SampleMapper;
 import org.murimoa.mapper.TimeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import lombok.extern.java.Log;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
     locations ={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@Log
 public class DBTest {
 	
     @Autowired
@@ -42,5 +47,18 @@ public class DBTest {
     @Test
     public void test3() {
         System.out.println(timeMapper.getTime());
+    }
+    
+    @Autowired
+    private SampleMapper mapper;
+    
+    @Test
+    public void test4() {
+        
+        log.info("" + mapper);
+        
+        SampleDTO dto = new SampleDTO();
+        dto.setTest("Test" + System.currentTimeMillis());
+        mapper.insert(dto);
     }
 }
